e025e6378617f41dfcd548d28ae78081f5dce46d
Anastasia Gordeeva <ana.gordeewa@bk.ru>
2020-05-10 23:11:17 +0300

    "two"

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x, y;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x + y);
         }
     }
 }
